7c976db63aba7ea724eae1719e09ff494211d81f
Konrad Soltys <konrad@mbp-konrad.home>
2021-01-11 12:25:30 +0100

    add isSelected method to Checkbox_object

--- wrappers/Checkbox_object.js
+++ wrappers/Checkbox_object.js
@@ -1,18 +1,36 @@
 var Wrapper = require('./Wrapper');
 
+/**
+ * Representing browser Checkbox object
+ * @class
+ */
 class Checkbox_object extends Wrapper{
   constructor(selector) {
     super(selector);
   }
 
-  check() { //TODO refactor with nice retry and error message
-    console.log('Checking Checkbox!');
-    if (this.element.getAttribute('value') === 'true') 
+/**
+ * selecting checkbox
+ * if checkbox is already selected nothing will happend 
+ * @function
+ */
+  check() {
+    console.log('>>> Checking Checkbox!');
+    if (this.isSelected()) 
       return;
     this.element.click();
     browser.pause(1000);
     if (this.element.getAttribute('value') === 'false') 
       this.element.click();
+  }
+
+  /**
+ * checking if checkbox is selected (checked)
+ * @function
+ * @return {boolean}
+ */
+  isSelected() {
+    return $(this.selector).getValue() === true;
   }
     
 }

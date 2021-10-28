fb6838daf5c1489c7e6ca144a2674326011e251a
konrad <konrad@tyk.io>
2021-10-28 16:54:55 +0200

    fix in dropdown

--- wrappers/DropDown_object.js
+++ wrappers/DropDown_object.js
@@ -9,8 +9,12 @@ class DropDown_object extends Wrapper{
   
   constructor(selector) {
     super(selector);
-    this.isDropDownHaveSelectTagName = $(this.selector).getTagName() === this.selectorTag; 
-    this.optionTagName =  (this.isDropDownHaveSelectTagName) ? "option" : "li";
+    if ($(this.selector).isExisting()) {
+      this.isDropDownHaveSelectTagName = $(this.selector).getTagName() === this.selectorTag; 
+      this.optionTagName =  (this.isDropDownHaveSelectTagName) ? "option" : "li";
+      return;
+    }
+    this.optionTagName = "li";
   }
 
 /**
